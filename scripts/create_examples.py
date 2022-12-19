6d58c34abc0098ac4d79b80f4e4744676c921fbf
Paola Holleway <paolaholleway@gmail.com>
2022-12-19 16:57:24 -0600

    refactor updates

--- scripts/create_examples.py
+++ scripts/create_examples.py
@@ -12,19 +12,19 @@ examples = root / "examples"
 data_files = root / "tests" / "data-files"
 
 catalog = Catalog(id="usda-cdl", description="An example catalog for USDA CDL data")
-for collection_type in AssetType:
-    collection = stac.create_collection(collection_type)
+for item_type in AssetType:
+    collection = stac.create_collection(item_type)
     catalog.add_child(collection)
-    if collection_type == AssetType.CDL:
+    if item_type == AssetType.Cropland:
         item = stac.create_cropland_item(str(data_files / "basic_cropland_2019.tif"))
-    elif collection_type == AssetType.Frequency:
+    elif item_type == AssetType.Corn:
         item = stac.create_frequency_item(
             str(data_files / "frequency_corn_2021.tif"),
             str(data_files / "frequency_cotton_2021.tif"),
             str(data_files / "frequency_soybean_2021.tif"),
             str(data_files / "frequency_wheat_2021.tif"),
         )
-    elif collection_type == AssetType.Cultivated:
+    elif item_type == AssetType.Cultivated:
         item = stac.create_cultivated_item(
             str(data_files / "ancillary_cultivated_2021.tif")
         )
